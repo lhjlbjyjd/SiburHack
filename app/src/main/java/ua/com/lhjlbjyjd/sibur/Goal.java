d813404218e125d8971340eecd9f1888f3c26873
Toreno <kostet499@gmail.com>
2017-11-25 21:08:12 +0300

    K

--- app/src/main/java/ua/com/lhjlbjyjd/sibur/Goal.java
+++ app/src/main/java/ua/com/lhjlbjyjd/sibur/Goal.java
@@ -6,8 +6,10 @@ package ua.com.lhjlbjyjd.sibur;
 
 public class Goal {
     private boolean state;
+    private String description;
 
     Goal(String description, boolean state) {
         this.state = state;
+        this.description = description;
     }
 }
