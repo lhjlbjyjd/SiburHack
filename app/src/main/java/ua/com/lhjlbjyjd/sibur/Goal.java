c2167f36b970f181e4f00a0f051d38c9be72f196
Toreno <kostet499@gmail.com>
2017-11-25 21:06:03 +0300

    K

---
+++ app/src/main/java/ua/com/lhjlbjyjd/sibur/Goal.java
@@ -0,0 +1,13 @@
+package ua.com.lhjlbjyjd.sibur;
+
+/**
+ * Created by Toreno on 25.11.2017.
+ */
+
+public class Goal {
+    private boolean state;
+
+    Goal(String description, boolean state) {
+        this.state = state;
+    }
+}
--- app/src/main/java/ua/com/lhjlbjyjd/sibur/MyAdapter.java
+++ app/src/main/java/ua/com/lhjlbjyjd/sibur/MyAdapter.java
@@ -1,5 +1,6 @@
 package ua.com.lhjlbjyjd.sibur;
 
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
